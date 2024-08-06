0be5786eb4c00368950274d8095cb29eb64d4eb9
k22036 <131113333+k22036@users.noreply.github.com>
2024-08-06 11:53:28 +0900

    change: sample

--- app.py
+++ app.py
@@ -2,25 +2,31 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/submit', methods=['POST'])
 def submit():
     choice = request.form['choice']
-    if choice == '現在利用している':
+    if choice == 'inUse':
         return redirect(url_for('user'))
     else:
         return redirect(url_for('notuser'))
 
+
 @app.route('/user')
 def user():
-    return render_template('user.html')#'ゲーム大好き!'
+    # 'ゲーム大好き!'
+    return render_template('branch.html', question='質問1', answer1='答え1', answer2='答え2')
+
 
 @app.route('/notuser')
 def notuser():
-    return render_template('notuser.html')#'ゲームやってみたいな!'
+    return render_template('notuser.html')  # 'ゲームやってみたいな!'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
